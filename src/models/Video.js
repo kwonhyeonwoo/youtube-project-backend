23d9aa1b2ddf7e2d92f6fbf812712786e12c1110
keonhyeonwoo <khw19980926@gmail.com>
2024-02-26 11:23:41 +0900

    fix

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const videoSchema = new mongoose.Schema({
     videoUrl: { type: String },
     title: { type: String, minlength: 5, maxlength: 15, required: true },
-    description: { type: String, maxlength: 30, required: true },
+    description: { type: String, maxlength: 10, maxlength: 200, required: true },
     hashtags: [{ type: String, maxlength: 10, }],
     meta: {
         views: { default: 0, type: Number },
